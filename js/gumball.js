15f303d66208c1238eb49e4d7c04059eb1ff2fd8
Kevin Kelly <kvinklly@gmail.com>
2017-11-11 19:42:51 -0600

    added horizontal movement

--- js/gumball.js
+++ js/gumball.js
@@ -15,7 +15,7 @@ function Gumball(x, y, radius, color) {
   this.x = x || random(this.radius, width-this.radius);
   this.y = y || (0 - this.radius);
   this.color = color || colors[floor(random(colors.length))];
-  this.velocity = createVector(0, 0);
+  this.velocity = createVector(random(-3, 3), 0);
 
   this.show = function () {
     noStroke();
@@ -31,8 +31,12 @@ function Gumball(x, y, radius, color) {
   }
 
   this.wallCollide = function () {
-    if (this.x - this.radius < 0 || this.x + this.radius > width) {
+    if (this.x - this.radius < 0) {
       this.velocity.x = -(this.velocity.x * DAMPENING_EFFECT);
+      this.x = 0 + this.radius;
+    } else if (this.x + this.radius > width) {
+      this.velocity.x = -(this.velocity.x * DAMPENING_EFFECT);
+      this.x = width - this.radius;
     } else if (this.y + this.radius > height) {
       this.velocity.y = -(this.velocity.y * DAMPENING_EFFECT);
       if (this.y + this.radius > height) {
